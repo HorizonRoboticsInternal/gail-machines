1976fb6bcf5f749a93aa9ba50b9141e182b5014e
Break Yang <breakds@gmail.com>
2023-12-15 17:29:24 -0800

    Update credential

--- machines/stormveil/services/cloudflare.nix
+++ machines/stormveil/services/cloudflare.nix
@@ -6,7 +6,7 @@
       enable = true;
       tunnels = {
         "e683fc73-ccf0-4278-97d7-c5a6ae8abf81" = {
-          credentialsFile = "/home/breakds/.cloudflared/e683fc73-ccf0-4278-97d7-c5a6ae8abf81.json";
+          credentialsFile = "/var/lib/cloudflare/e683fc73-ccf0-4278-97d7-c5a6ae8abf81.json";
           ingress = {
             "*.breakds.net" = "http://localhost:80";
           };
