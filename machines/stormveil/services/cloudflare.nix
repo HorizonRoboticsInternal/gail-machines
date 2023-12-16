ac70b3ab85b4426e39388a28e9c4c15e2a1a459b
Break Yang <breakds@gmail.com>
2023-12-15 17:26:41 -0800

    Add tunnel

--- machines/stormveil/services/cloudflare.nix
+++ machines/stormveil/services/cloudflare.nix
@@ -4,6 +4,15 @@
   config = {
     services.cloudflared = {
       enable = true;
+      tunnels = {
+        "e683fc73-ccf0-4278-97d7-c5a6ae8abf81" = {
+          credentialsFile = "/home/breakds/.cloudflared/e683fc73-ccf0-4278-97d7-c5a6ae8abf81.json";
+          ingress = {
+            "*.breakds.net" = "http://localhost:80";
+          };
+          default = "http_status:404";
+        };
+      };
     };
   };
 }
