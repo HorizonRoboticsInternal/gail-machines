9d310cea2f0e268adb984a518f1ef12247fc1c94
Break Yang <breakds@gmail.com>
2023-12-15 18:10:23 -0800

    test 2

--- machines/stormveil/services/cloudflare.nix
+++ machines/stormveil/services/cloudflare.nix
@@ -8,7 +8,7 @@
         "e683fc73-ccf0-4278-97d7-c5a6ae8abf81" = {
           credentialsFile = "/var/lib/cloudflare/e683fc73-ccf0-4278-97d7-c5a6ae8abf81.json";
           ingress = {
-            "*.breakds.net" = "http://localhost:2345";
+            "gaildata.breakds.net" = "http://localhost:2345";
           };
           default = "http_status:404";
         };
