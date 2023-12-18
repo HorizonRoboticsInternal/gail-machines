bee6894752db34fdab5a72323c380311b41b070e
Break Yang <breakds@gmail.com>
2023-12-18 14:26:34 -0800

    Specify filerun version

--- machines/stormveil/services/filerun.nix
+++ machines/stormveil/services/filerun.nix
@@ -41,7 +41,7 @@
 
     # The backend and web app (Filerun)
     virtualisation.oci-containers.containers."filerun" = {
-      image = "filerun/filerun";
+      image = "filerun/filerun:8.1";
       environment = {
         "FR_DB_HOST" = "${dbContainerHost}";
         # This is the default port that mariadb runs at.
