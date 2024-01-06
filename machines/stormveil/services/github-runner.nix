fb4480f69b3ec65c2541292000ff388897675012
Break Yang <breakds@gmail.com>
2024-01-05 18:22:16 -0800

    Add another runner

--- machines/stormveil/services/github-runner.nix
+++ machines/stormveil/services/github-runner.nix
@@ -1,3 +1,8 @@
+# To add a new runner, go to GitHub page of Hobot, click Settings > Actions >
+# Runners, and then click to create a new runner. The only thing you need to do
+# manually is find the token and save it to a toke file. Duplicate the runner
+# block below, and just modify the name and toke file and you are all set.
+
 { config, lib, pkgs, ... }:
 
 {
@@ -12,6 +17,21 @@
       replace = true;  # Replace any existing runner with the same name.
       url = "https://github.com/HorizonRoboticsInternal/Hobot";
       tokenFile = "/home/breakds/.config/github/runner_registration_token.001";
+
+      extraPackages = [ pkgs.docker ];
+      extraLabels = [ "stormveil" ];
+    };
+
+    services.github-runners."Hobot002" = {
+      name = "STORMVEIL-HOBOT-2";
+      
+      # Leave `user` and `workDir` as null (i.e. not configured) so that they
+      # will be dynamically created at runtime.
+      
+      enable = true;
+      replace = true;  # Replace any existing runner with the same name.
+      url = "https://github.com/HorizonRoboticsInternal/Hobot";
+      tokenFile = "/home/breakds/.config/github/runner_registration_token.002";
 
       extraPackages = [ pkgs.docker ];
       extraLabels = [ "stormveil" ];
